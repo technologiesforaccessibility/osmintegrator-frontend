11ccfc1663ba9336137f385935bc84da2a96c292
Adam <adamsarach@gmail.com>
2021-02-02 18:18:22 +0100

    Adjusted components height in main view and authorization forms

--- src/components/DashboardSiderbar.js
+++ src/components/DashboardSiderbar.js
@@ -41,7 +41,7 @@ class DashboardSiderbar extends Component {
 
 
                                 <h6 className="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-                                    <span>Last used tales</span>
+                                    <span>Last used tiles</span>
                                     <a className="link-secondary" href="/" aria-label="Add a new report">
                                         <span data-feather="plus-circle"></span>
                                     </a>
