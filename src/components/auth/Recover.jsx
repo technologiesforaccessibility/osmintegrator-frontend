ec6cffeb4e08386afbec27c953678adc4b09b3cf
Adam <adamsarach@gmail.com>
2021-06-13 18:11:37 +0200

    Auth components changed into functional

--- src/components/auth/Recover.jsx
+++ src/components/auth/Recover.jsx
@@ -1,95 +1,70 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
+import {useFormik} from 'formik';
+import {useTranslation} from 'react-i18next';
 
-import {sentResetLinkText} from '../../utilities/utilities-texts';
 import {noTokenHeaders} from '../../config/apiConfig';
 import client from '../../api/apiInstance';
-import {unsafeLoginApiError} from '../../utilities/utilities';
+import {unsafeFormApiError} from '../../utilities/utilities';
 
 import '../../stylesheets/recover.scss';
 import colors from '../../stylesheets/colors.module.scss';
 
-class Recover extends Component {
-    constructor(props) {
-        super(props);
 
-        this.state = {
-            email: '',
-            isMessageShown: false,
-            message: null,
-            messageColor: '',
-        };
+const Recover = () => {
+  const {t} = useTranslation();
 
-        this.change = this.change.bind(this);
-        this.submit = this.submit.bind(this);
+  const [message, setMessage] = useState(null);
+  const [messageColor, setMessageColor] = useState(null);
+
+  const formik = useFormik({
+    initialValues: {
+      recoverEmail: '',
+    },
+    onSubmit: ({recoverEmail}) => {
+      runRecover(recoverEmail);
+    },
+  });
+
+  const runRecover = async email => {
+    try {
+      await client.api.accountForgotPasswordCreate(
+        {
+          email,
+        },
+        {headers: noTokenHeaders()},
+      );
+      setMessageColor(colors['colorMessageSuccess']);
+      setMessage(t('recover.emailSent'));
+    } catch (error) {
+      setMessageColor(colors['colorMessageFail']);
+      setMessage(unsafeFormApiError(error, t, 'recover'));
     }
+  };
 
-    change(e) {
-        this.setState({
-            [e.target.id]: e.target.value,
-        });
-    }
-
-    setMessageColor = color => {
-        this.setState({messageColor: color});
-    };
-
-    updateMessage = message => {
-        this.setState({
-            message: `${message === undefined ? message : null}`
-        });
-    };
-
-    async submit(e) {
-        e.preventDefault();
-
-        try {
-            await client.api.accountForgotPasswordCreate(
-                {
-                    email: this.state.recoverEmail,
-                },
-                {headers: noTokenHeaders()},
-            );
-            this.setMessageColor(colors['colorMessageSuccess']);
-            this.updateMessage(sentResetLinkText());
-        } catch (error) {
-            this.setMessageColor(colors['colorMessageFail']);
-            this.updateMessage('Cannot send reset email');
-            unsafeLoginApiError(error);
-        }
-    }
-
-    render() {
-        return (
-            <React.Fragment>
-                <h1 className="auth-title">Recover your password</h1>
-                <form name="recover-form" onSubmit={e => this.submit(e)}>
-                    <div className="inputbox-spacer">
-                        <input
-                            type="text"
-                            id="recoverEmail"
-                            placeholder="Email"
-                            onChange={e => {
-                                this.change(e);
-                            }}
-                            onClick={this.updateMessage()}
-                        />
-                    </div>
-                    <div className="recover-button-area">
-                        <button type="submit" id="button-reset">
-                            Reset your password
-                        </button>
-                    </div>
-                </form>
-                <div className="centered auth-info-placeholder" value="">
-                    {this.state.message && (
-                        <span style={{color: this.state.messageColor}}>
-                            {this.state.message}
-                        </span>
-                    )}
-                </div>
-            </React.Fragment>
-        );
-    }
-}
+  return (
+    <>
+      <h1 className="auth-title">{t('recover.title')}</h1>
+      <form onSubmit={formik.handleSubmit}>
+        <div className="inputbox-spacer">
+          <input
+            type="text"
+            id="recoverEmail"
+            placeholder="Email"
+            onChange={formik.handleChange}
+            value={formik.values.recoverEmail}
+          />
+        </div>
+        <div className="recover-button-area">
+          <button type="submit" id="button-reset">
+            {t('recover.button')}
+          </button>
+        </div>
+      </form>
+      <div className="centered auth-info-placeholder">
+        {message && <span style={{color: messageColor}}>{message}</span>}
+      </div>
+    </>
+  );
+};
 
 export default Recover;
