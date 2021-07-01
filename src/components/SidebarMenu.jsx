3d503b3a3a0d370cc3665f32e982c638287071ad
AdamSarach <44923365+AdamSarach@users.noreply.github.com>
2021-07-01 19:01:41 +0200

    User context created (#43)

--- src/components/SidebarMenu.jsx
+++ src/components/SidebarMenu.jsx
@@ -1,27 +1,22 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {useTranslation} from 'react-i18next';
 
 import SidebarListItem from './SidebarListItem';
+import {UserContext} from './contexts/UserContextProvider';
 
 const {REACT_APP_CONTACT_FORM} = process.env;
 
 const SidebarMenu = () => {
   const {t} = useTranslation();
+  const {isUnsafeAuthorized} = useContext(UserContext);
 
   return (
     <ul className="nav flex-column">
       <SidebarListItem name="Map" link="/" />
-      <SidebarListItem
-        name="Profile"
-        link="/profile"
-      />
-      <SidebarListItem name="History"  link="/" />
-      <SidebarListItem externalLink name={t('sidebar.contact')}  link={REACT_APP_CONTACT_FORM} />
-      <SidebarListItem
-        name="Management dashboard"
-        dataFeather="layers"
-        link="/manage"
-      />
+      <SidebarListItem name="Profile" link="/profile" />
+      <SidebarListItem name="History" link="/" />
+      <SidebarListItem externalLink name={t('sidebar.contact')} link={REACT_APP_CONTACT_FORM} />
+      {isUnsafeAuthorized && <SidebarListItem name="Management dashboard" link="/manage" />}
     </ul>
   );
 };
