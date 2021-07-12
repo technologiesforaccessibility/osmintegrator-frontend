7cc041efdfdadce3c2759aeb64103bc33848d729
Przemysław Stochel <przemek.stochel@revolve.pro>
2021-07-12 16:55:01 +0200

    PT-90 Dashboard rewritten into functional component

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,6 +1,6 @@
-import React, {Component} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Route, Switch} from 'react-router-dom';
-import {connect} from "react-redux";
+import {useDispatch, useSelector} from 'react-redux';
 
 import DashboardHeader from './DashboardHeader';
 import DashboardSiderbar from './DashboardSiderbar';
@@ -8,62 +8,51 @@ import DashboardMain from './DashboardMain';
 import ProfileRouter from './ProfileRouter';
 import ManagementPanel from './ManagementPanel';
 import {validateLogin} from '../redux/actions/authActions';
+import {selectAuthIsLoggedIn} from '../redux/selectors/authSelector';
 
 import '../stylesheets/dashboard.scss';
 
-class Dashboard extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      propertyGrid: null,
-    };
+export default function Dashboard() {
+  const [propertyGrid, setPropertyGrid] = useState(null);
+
+  const isLoggedIn = useSelector(selectAuthIsLoggedIn);
+  const dispatch = useDispatch();
+
+
+  useEffect(() => {
+    dispatch(validateLogin());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  function updatePropertyGrid(newGrid) {
+    setPropertyGrid(newGrid);
   }
 
-  componentDidMount() {
-    this.props.validateLogin();
-  }
+  return (
+    <React.Fragment>
+      <DashboardHeader isLoggedIn={isLoggedIn} />
 
-  updatePropertyGrid = newGrid => {
-    this.setState({propertyGrid: newGrid});
-  };
-
-  render() {
-    return (
-      <React.Fragment>
-        <DashboardHeader isLoggedIn={this.props.auth.isLoggedIn} />
-
-        <div className="container-fluid">
-          <div className="row">
-            <DashboardSiderbar
-              isLoggedIn={this.props.auth.isLoggedIn}
-              connectBusStops={this.canConnectBusStops}
-              updatePropertyGrid={this.updatePropertyGrid}
-            />
-            <Switch>
-              <Route path="/profile" component={ProfileRouter} />
-              <Route path="/manage" component={ManagementPanel} />
-              <Route path="/" render={() => <DashboardMain />} />
-            </Switch>
-          </div>
+      <div className="container-fluid">
+        <div className="row">
+          <DashboardSiderbar
+            isLoggedIn={isLoggedIn}
+            propertyGrid={propertyGrid}
+            updatePropertyGrid={updatePropertyGrid}
+          />
+          <Switch>
+            <Route path="/profile" component={ProfileRouter} />
+            <Route path="/manage" component={ManagementPanel} />
+            <Route path="/" render={() => <DashboardMain />} />
+          </Switch>
         </div>
+      </div>
 
 
-        <script
-          src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js"
-          integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE"
-          crossOrigin="anonymous"
-        />
-      </React.Fragment>
-    );
-  }
+      <script
+        src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js"
+        integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE"
+        crossOrigin="anonymous"
+      />
+    </React.Fragment>
+  );
 }
-
-const mapStateToProps = (state) => ({
-  auth: state.auth
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  validateLogin: () => dispatch(validateLogin())
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
