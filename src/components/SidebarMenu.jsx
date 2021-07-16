03c55cd934f9d4559c1c644a7b8e6df20625315b
Przemysław Stochel <przemek.stochel@revolve.pro>
2021-07-16 15:03:29 +0200

    PT-100 sidebar adjustments

--- src/components/SidebarMenu.jsx
+++ src/components/SidebarMenu.jsx
@@ -1,15 +1,13 @@
-import React, {useContext} from 'react';
+import React from 'react';
 import {useTranslation} from 'react-i18next';
 
 import SidebarListItem from './SidebarListItem';
-import {UserContext} from './contexts/UserContextProvider';
 import {paths} from '../utilities/constants';
 
 const {REACT_APP_CONTACT_FORM} = process.env;
 
 const SidebarMenu = () => {
   const {t} = useTranslation();
-  const {isUnsafeAuthorized} = useContext(UserContext);
 
   return (
     <ul className="nav flex-column">
@@ -17,7 +15,7 @@ const SidebarMenu = () => {
       <SidebarListItem name="Profile" link={paths.PROFILE} />
       <SidebarListItem name="History" link={paths.HOME} />
       <SidebarListItem externalLink name={t('sidebar.contact')} link={REACT_APP_CONTACT_FORM} />
-      {isUnsafeAuthorized && <SidebarListItem name="Management dashboard" link={paths.MANAGEMENT_PANEL} />}
+      <SidebarListItem name="Management dashboard" link={paths.MANAGEMENT_PANEL} />
     </ul>
   );
 };
