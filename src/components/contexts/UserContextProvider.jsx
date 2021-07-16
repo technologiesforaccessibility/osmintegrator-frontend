7587f3465ed981be34ca0193a3b7624eb57d19f4
Przemysław Stochel <przemek.stochel@revolve.pro>
2021-07-16 14:40:58 +0200

    PT-100 validate login adjustments

--- src/components/contexts/UserContextProvider.jsx
+++ src/components/contexts/UserContextProvider.jsx
@@ -6,16 +6,25 @@ import {validateLogin} from '../../redux/actions/authActions';
 export const UserContext = createContext();
 
 const UserContextProvider = ({children}) => {
+    const [isLoading, setIsLoading] = useState(true);
     const token = useSelector(selectAuthToken);
     const dispatch = useDispatch();
 
     useEffect(() => {
-        dispatch(validateLogin());
+        if (token) {
+            dispatch(validateLogin()).finally(() => setIsLoading(false));
+        } else {
+            setIsLoading(false);
+        }
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [token]);
 
     //Todo: change name and change value to false
     const [isUnsafeAuthorized, setIsUnsafeAuthorized] = useState(true);
+
+    if (isLoading) {
+        return <></>;
+    }
 
     return (
         <UserContext.Provider value={{isUnsafeAuthorized, setIsUnsafeAuthorized}}>
