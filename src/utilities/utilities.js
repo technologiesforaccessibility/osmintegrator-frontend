0d3ecbbf02c3ce0e3ec8b52f348885b63e81d29b
Adam <adamsarach@gmail.com>
2021-09-05 20:14:36 +0200

    Report icon changed

--- src/utilities/utilities.js
+++ src/utilities/utilities.js
@@ -10,13 +10,19 @@ import iconYellow from '../assets/bus_stop_icon_yellow.png';
 import iconViolet from '../assets/bus_stop_icon_violet.png';
 import iconBlue from '../assets/bus_stop_icon_blue.png';
 import iconOrange from '../assets/bus_stop_icon_orange_new.png';
+import reporBlackIcon from '../assets/report_black.png';
+import reportBlueIcon from '../assets/report_blue.png';
+import reportGreenIcon from '../assets/report_green.png';
+import reportRedIcon from '../assets/report_red.png';
+import reportGreyIcon from '../assets/report_grey.png';
+
 
 const reportIcons = {
-  initial: iconOrange,
-  created: iconYellow,
-  approved: iconGreen,
-  rejected: iconViolet,
-  unexpected: iconBlue,
+  initial: reportGreyIcon,
+  created: reportBlueIcon,
+  approved: reportGreenIcon,
+  rejected: reportRedIcon,
+  unexpected: reporBlackIcon,
 };
 
 const comparePasswords = (pass1, pass2) => {
