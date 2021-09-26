4d6c2036e58a2b83c1a68c4f69c69da1d0a1e3c6
Lukasz Taraszka <luktar@gmail.com>
2021-09-26 21:43:19 +0200

    Fixed validation problem

--- src/utilities/validationSchema.js
+++ src/utilities/validationSchema.js
@@ -4,7 +4,7 @@ import {object, string, ref} from 'yup';
 
 export const RegisterSchema = object().shape({
   username: string().min(3, i18n.t('register.usernamePrompt')).required(i18n.t('register.usernameRequiredError')),
-  email: string().email(i18n.t('register.emailInvalidError')).required('register.emailRequiredError'),
+  email: string().email(i18n.t('register.emailInvalidError')).required(i18n.t('register.emailRequiredError')),
   password1: string().min(8, i18n.t('register.passwordPrompt')).required(i18n.t('register.passwordPlaceholder')),
   password2: string()
     .oneOf([ref('password1')], i18n.t('register.passwordRefError'))
