9c8069c2f123fd6dd01863dbf069ba342fbef01e
Marcin <marcin.niemireski@revolve.pro>
2021-10-13 13:47:25 +0200

    Add new logos

--- src/components/AuthContainer.jsx
+++ src/components/AuthContainer.jsx
@@ -1,7 +1,9 @@
 import Footer from './Footer';
 import dots from './../assets/authDots.png';
 import logo from './../assets/OsmIntegrator.png';
+import rdnLogo from './../assets/logos/rdn-logo.png';
 import {paths} from '../utilities/constants';
+import {ReactComponent as RevolveLogo} from './../assets/logos/revolve-logo.svg';
 
 import '../stylesheets/auth.scss';
 
@@ -12,13 +14,21 @@ export default function AuthContainer({children}) {
         <img src={logo} alt="logo" />
       </a>
       <div className="auth__container">
-        <div className="auth__dots">
+        <div className="auth__dots auth__dots--left ">
           <img src={dots} className="auth__image--left" alt="BackgroundLeft"></img>
         </div>
         <div className="auth__form-area">
           <div className="auth__form-box">{children}</div>
         </div>
-        <div className="auth__dots">
+        <div className="auth__logos">
+          <a className="auth__logos--rdn" href="https://rozwiazaniadlaniewidomych.org/">
+            <img src={rdnLogo} alt="Logo Rozwiązania dla niewidomych" />
+          </a>
+          <a className="auth__logos--revolve" href="https://revolve.healthcare/">
+            <RevolveLogo />
+          </a>
+        </div>
+        <div className="auth__dots auth__dots--right ">
           <img src={dots} className="auth__image--right" alt="BackgroundRight"></img>
         </div>
       </div>
