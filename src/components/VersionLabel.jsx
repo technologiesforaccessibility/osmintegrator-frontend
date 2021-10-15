4252fed036369653dfcaca5d76590baec56f6085
Marcin <marcin.niemireski@revolve.pro>
2021-10-15 12:26:10 +0200

    Display current backend version

--- src/components/VersionLabel.jsx
+++ src/components/VersionLabel.jsx
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {useTranslation} from 'react-i18next';
 import * as pack from '../../package.json';
+import api from '../api/apiInstance';
+import {basicHeaders} from '../config/apiConfig';
 
 const VersionLabel = () => {
   const {t} = useTranslation();
+  const [version, setVersion] = useState('');
+  const [err, setErr] = useState(false);
+
+  useEffect(() => {
+    async function getVersion() {
+      try {
+        setErr(false);
+        const response = await api.versionList({
+          headers: basicHeaders(),
+        });
+        setVersion(response.data);
+      } catch (error) {
+        setErr(true);
+      }
+    }
+    getVersion();
+  }, []);
 
   return (
     <>
       {t('version')}
-      {pack.version}
+      <span>F: {pack.version}</span>
+      {!err && <span>, B: {version}</span>}
     </>
   );
 };
