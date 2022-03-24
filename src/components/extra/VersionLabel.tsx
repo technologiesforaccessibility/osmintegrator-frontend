87f37be2a5cee4083ea42528d3702f45a32d2a1d
Michał Drabik <micdra7@gmail.com>
2022-03-24 09:01:22 +0100

    fix version panel

--- src/components/extra/VersionLabel.tsx
+++ src/components/extra/VersionLabel.tsx
@@ -2,17 +2,16 @@ import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
 import { selectVersion } from 'redux/selectors/authSelector';
 
-import * as pack from '../../../package.json';
-
 const VersionLabel = () => {
   const { t } = useTranslation();
 
   const version = useSelector(selectVersion);
+  const packageVersion = process.env.REACT_APP_VERSION;
 
   return (
     <>
       {t('version')}
-      <span>{pack.version}</span>
+      <span>{packageVersion}</span>
       {version && <span> ({version}</span>})
     </>
   );
